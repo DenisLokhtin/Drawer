489c2844d7cab21a3e5497eeaded9646127aad8e
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-11-28 23:32:45 +0600

    сделал рисование кругом, сделал выбор цветов

---
+++ back/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const {nanoid} = require('nanoid');
+const app = express();
+require('express-ws')(app);
+
+const port = 8000;
+
+app.use(cors());
+
+const activeConnections = {};
+
+app.ws('/chat', function (ws, req) {
+    const id = nanoid(10);
+    activeConnections[id] = ws;
+
+    console.log(`Client connected! id=${id}`);
+
+    ws.on('message', msg => {
+        ws.send(msg);
+    });
+
+    ws.on('close', () => {
+        console.log(`Client disconnected! id=${id}`);
+
+        delete activeConnections[id];
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+});
--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useRef} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
+    const [state, setState] = useState({
+        mouseDown: false,
+        pixelsArray: []
+    });
+
+    const [color, setColor] = useState('black');
+
+    const canvas = useRef(null);
+
+    const canvasMouseMoveHandler = event => {
+        if (state.mouseDown) {
+            event.persist();
+            const clientX = event.clientX;
+            const clientY = event.clientY;
+
+            setState(prevState => {
+                return {
+                    ...prevState,
+                    pixelsArray: [...prevState.pixelsArray, {
+                        x: clientX,
+                        y: clientY
+                    }]
+                };
+            });
+
+            const context = canvas.current.getContext('2d');
+            context.beginPath();
+            context.arc(clientX - 13, clientY - 20, 10, 0, 2*Math.PI, false);
+            context.fillStyle = color;
+            context.fill();
+        }
+    };
+
+
+    const mouseDownHandler = event => {
+        setState({...state, mouseDown: true});
+    };
+
+
+    const mouseUpHandler = event => {
+        setState({...state, mouseDown: false, pixelsArray: []});
+    };
+
+    const colorSet = color => {
+        setColor(color);
+    };
+
+
+    return (
+        <div>
+            <canvas
+                ref={canvas}
+                style={{border: '2px solid black'}}
+                width={1600}
+                height={900}
+                onMouseDown={mouseDownHandler}
+                onMouseUp={mouseUpHandler}
+                onMouseMove={canvasMouseMoveHandler}
+            />
+            <div className="colors">
+                <div onClick={event => colorSet(event.target.className)} className="black"/>
+                <div onClick={event => colorSet(event.target.className)} className="white"/>
+                <div onClick={event => colorSet(event.target.className)} className="green"/>
+                <div onClick={event => colorSet(event.target.className)} className="blue"/>
+                <div onClick={event => colorSet(event.target.className)} className="red"/>
+                <div onClick={event => colorSet(event.target.className)} className="yellow"/>
+            </div>
+        </div>
+    );
+
+};
 export default App;
+
